dbe71d7f573db069336f667779979880c2134434
Bas van Dijk <v.dijk.bas@gmail.com>
2011-03-05 00:53:33 +0100

    Use case-insensitive instead of CIAscii and related functions Derived a FoldCase instance for Ascii using GeneralizedNewtypeDeriving

--- Data/Ascii.hs
+++ Data/Ascii.hs
@@ -2,7 +2,6 @@
 module Data.Ascii
     ( -- * Datatypes
       Ascii
-    , CIAscii
       -- * Construction
       -- ** Safe
     , fromByteString
@@ -16,42 +15,22 @@ module Data.Ascii
     , toByteString
     , toString
     , toText
-      -- * Case insensitive
-    , toCIAscii
-    , fromCIAscii
-    , ciToByteString
     ) where
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as S8
-import Data.Char (toLower, isAscii)
+import Data.Char (isAscii)
 import Data.String (IsString (..))
 import Data.Data (Data)
 import Data.Typeable (Typeable)
 import Data.Text (Text)
+import Data.CaseInsensitive (FoldCase)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 
 newtype Ascii = Ascii ByteString
-    deriving (Show, Eq, Read, Ord, Data, Typeable, IsString)
-
-data CIAscii = CIAscii
-    { ciOriginal :: !ByteString
-    , ciLowerCase :: !ByteString
-    }
-    deriving (Data, Typeable)
-
-instance Show CIAscii where
-    show = show . ciOriginal
-instance Read CIAscii where
-    readsPrec i = map (\(x, y) -> (toCIAscii x, y)) . readsPrec i
-instance Eq CIAscii where
-    x == y = ciLowerCase x == ciLowerCase y
-instance Ord CIAscii where
-    x <= y = ciLowerCase x <= ciLowerCase y
-instance IsString CIAscii where
-    fromString = toCIAscii . unsafeFromString
+    deriving (Show, Eq, Read, Ord, Data, Typeable, IsString, FoldCase)
 
 fromByteString :: ByteString -> Maybe Ascii
 fromByteString bs
@@ -78,12 +57,6 @@ unsafeFromString = Ascii . S8.pack
 unsafeFromText :: Text -> Ascii
 unsafeFromText = Ascii . TE.encodeUtf8
 
-toCIAscii :: Ascii -> CIAscii
-toCIAscii (Ascii bs) = CIAscii bs $ S8.map toLower bs
-
-fromCIAscii :: CIAscii -> Ascii
-fromCIAscii = Ascii . ciOriginal
-
 toByteString :: Ascii -> ByteString
 toByteString (Ascii bs) = bs
 
@@ -92,6 +65,3 @@ toString (Ascii bs) = S8.unpack bs
 
 toText :: Ascii -> Text
 toText (Ascii bs) = TE.decodeASCII bs
-
-ciToByteString :: CIAscii -> ByteString
-ciToByteString = ciOriginal
